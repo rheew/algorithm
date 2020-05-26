cf3553aa4e547d9ace180aa9111b4104b8ef736f
rheew <47098896+rheew@users.noreply.github.com>
2020-05-26 21:47:19 +0900

    break and move
    BFS문제
    벽을 부수고 온 경로인지 저장하는것이 핵심
    BFS and greedy 혼합문제

---
+++ 우선순위 큐/Python/maxHeap-1.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+numbers = int(input())
+heap = []
+
+#Max Heap
+for _ in range(numbers):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap, (-num))
+    else:
+        try:
+            print(-1 * heapq.heappop(heap))
+        except:
+            print(0)
+            
